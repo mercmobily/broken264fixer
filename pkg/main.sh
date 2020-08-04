d8c2e70ffa065ef6a7eab5f7c113b55833f15526
Tony Mobily <tonymobily@gmail.com>
2020-08-04 09:18:43 +0800

    License

--- pkg/main.sh
+++ pkg/main.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# MIT License
+# Copyright (c) 2020 Tony Mobily
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 export LD_LIBRARY_PATH="../lib"
 
 # h264 is the compression, mp4 is a container.
@@ -45,7 +65,7 @@ cd $tmpDir
 #                      The mkv file will merge the compressed audio (.mp3)
 #                      with the extacted h264 file (.h264) using the created
 #                      timestamps
-../mkvmerge  --output "input.mkv" --timestamps "0:input.video.ts.txt" "input.h264" "input.wav"
+../mkvmerge  --output "input.mkv" --timestamps "0:input.video.ts.txt" "input.h264" "input.wav" # or "input.mp3" if created
 
 cd -
 mv "$tmpDir/input.mkv" "$USER_PWD"/"${inputBasename}".mkv
